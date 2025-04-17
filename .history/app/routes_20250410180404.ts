fadc3b95ed0521f0e692173ca0200914c118e10a
hana <72500715+hana4979@users.noreply.github.com>
2025-04-17 23:23:20 +0900

    Delete .history directory

--- .history/app/routes_20250410180404.ts
+++
@@ -1,7 +0,0 @@
-//라우트정의하는 파일이라고함
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("/Users/incheol/SkhuRing/skhuring-frontend/app/pages/home.tsx"), // 홈 페이지
-  route("/oauth/google/redirect", "/Users/incheol/SkhuRing/skhuring-frontend/app/components/common/GoogleAuth.tsx"), // 구글 로그인 페이지 추가
-] satisfies RouteConfig;
--- .history/app/routes_20250410180838.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes';
-
-export default [
-  index('./pages/home.tsx'),
-  route('/oauth/google/redirect', './components/common/GoogleAuth.tsx'),
-] satisfies RouteConfig;
