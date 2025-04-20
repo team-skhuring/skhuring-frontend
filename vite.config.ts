d12a63d1d54a51e8cdb60aea817638ad9c1b26b8
dldlscjf123 <dldlscjf1231@naver.com>
2025-04-20 16:32:45 +0900

    fix : 채팅 오류 고치기 완성

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   define: {
-    global: {},
+    global: 'globalThis',
   },
   plugins: [react(), tailwindcss()],
   server: {
